27160ee89b6d10e28ad57256fccdf4817072c519
Pratik Luitel <pluitel11@gmail.com>
2022-08-08 16:13:37 +0545

    QOL changes

--- tensorflow-serving/serve_super_resolution_model.sh
+++ tensorflow-serving/serve_super_resolution_model.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+docker pull tensorflow/serving && \
 docker run -t -p 8501:8501 \
     -v "$(pwd)/models/esrgan_super_resolution:/models/super_resolution/" \
     -e MODEL_NAME=super_resolution \
